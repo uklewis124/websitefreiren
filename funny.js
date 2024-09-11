db50cca2227c8f8fe1373634a726da35178d4ea6
Lewis Davies <lewisastondavies1@gmail.com>
2024-09-11 21:46:06 +0100

    Refactor fade animation and update menu styling

--- funny.js
+++ funny.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     const topImage = document.getElementById('top-image');
+    const menu = document.getElementById('menu');
     let opacity = 1;
+    let menuOpacity = 0;
     const fadeDuration = 1000;
     const fadeStep = 10;
     const finalOpacity = 0.2
@@ -10,6 +12,9 @@ window.onload = function() {
             if (opacity > finalOpacity) {
                 opacity -= (fadeStep / fadeDuration);
                 topImage.style.opacity = opacity;
+
+                menuOpacity += (fadeStep / fadeDuration);
+                menu.style.opacity = menuOpacity;
             } else {
                 clearInterval(fadeInterval);
             }
